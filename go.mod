3639f2c10dc85e36d746c5b3cda16e8a17c451c7
KouT127 <kou26haru@gmail.com>
2020-06-01 23:15:32 +0900

    modify: query a bill  from BigQuery

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/KouT127/billing-slack-notifier
 go 1.13
 
 require (
+	cloud.google.com/go/bigquery v1.8.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/slack-go/slack v0.6.4
 )
--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"cloud.google.com/go/bigquery"
+	"context"
 	"fmt"
 	"github.com/slack-go/slack"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
 	"log"
 	"os"
 )
 
 func main() {
 	client := NewSlackClient()
-	err := client.sendMessage("New Test")
-	if err != nil {
-		log.Fatalf("%v", err)
+
+	results := queryBill()
+	for _, result := range results {
+		err := client.sendMessage(result)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
 	}
 }
 
@@ -40,4 +48,51 @@ func (c *SlackClient) sendMessage(msg string) error {
 	}
 	fmt.Printf("channel: %s", channel)
 	return nil
+}
+
+func queryBill() []string {
+	ctx := context.Background()
+	json := os.Getenv("SERVICE_ACCOUNT_JSON")
+	projectID := os.Getenv("PROJECT_ID")
+
+	client, err := bigquery.NewClient(ctx, projectID, option.WithCredentialsJSON([]byte(json)))
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	query := fmt.Sprintf("SELECT "+
+		"invoice.month,"+
+		"SUM(cost)"+
+		"+ SUM(IFNULL((SELECT SUM(c.amount) "+
+		"FROM UNNEST(credits) c), 0))"+
+		"AS total, (SUM(CAST(cost * 1000000 AS int64)) + SUM(IFNULL((SELECT SUM(CAST(c.amount * 1000000 as int64)) "+
+		"FROM UNNEST(credits) c), 0))) / 1000000 "+
+		"AS total_exact "+
+		"FROM `%s.biling.gcp_billing_export_v1_01C98C_7E00E6_392CCC` "+
+		"GROUP BY 1 "+
+		"ORDER BY 1 ASC;", projectID)
+
+	q := client.Query(query)
+
+	rows, err := q.Read(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results := make([]string, rows.TotalRows)
+	idx := 0
+	for {
+		var values []bigquery.Value
+		err := rows.Next(&values)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		results[idx] = fmt.Sprintf("%s　利用金額: %f", values[0], values[1])
+		idx++
+	}
+
+	return results
 }
